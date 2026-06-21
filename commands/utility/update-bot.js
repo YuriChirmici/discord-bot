6f1c29a1cfb97d0f492b4d878e39a31d9cc2d91c
Yuri Kirmichi <ykirmichi4@mail.ru>
2026-06-21 17:42:22 +0300

    add update test command

---
+++ commands/utility/update-bot.js
@@ -0,0 +1,38 @@
+const {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+} = require("discord.js");
+const configService = require("../../services/config");
+const { exec } = require("child_process");
+
+const NAME = getCommandName(__filename);
+
+module.exports = {
+	name: NAME,
+	get() {
+		return new SlashCommandBuilder()
+			.setName(NAME)
+			.setDescription("Обновить бота (git pull + npm install + перезапуск)")
+			.setDefaultMemberPermissions(PermissionFlagsBits[configService.config.commandsPermission])
+			.setDMPermission(false);
+	},
+
+	async execute({ interaction }) {
+		await interaction.deferReply({ ephemeral: true });
+
+		exec("git pull && npm install --omit=dev", async (err, stdout, stderr) => {
+			if (err) {
+				logError(err, "Update command failed");
+				await interaction.editReply({ content: `Ошибка при обновлении:\n\`\`\`${stderr || err.message}\`\`\`` });
+				return;
+			}
+
+			await interaction.editReply({ content: `Обновление успешно:\n\`\`\`${stdout}\`\`\`\nПерезапускаю...` });
+
+			// Restart after reply is sent
+			setTimeout(() => {
+				process.exit(0);
+			}, 1000);
+		});
+	}
+};
